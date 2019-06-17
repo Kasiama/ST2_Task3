2cce7589c33d735f43dc98dc3e42a608279bec9c
Ivan <kasiama@yandex.ru>
2019-06-17 21:40:11 +0300

    init

---
+++ LoadedImages/MainView/New Group/TableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.h
+//  LoadedImages
+//
+//  Created by Иван on 6/15/19.
+//  Copyright © 2019 Иван. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
